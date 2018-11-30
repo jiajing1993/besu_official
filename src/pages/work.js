848bbd738ce4a86348e80ea8a9500c093e2f6b4e
Jia Jing <jiajing19932@gmail.com>
2018-11-30 17:48:48 +0800

    half way - doing work page

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Layout from '../components/layout'
 
+import '../styles/work.scss'
+
 const WorkPage = () => (
   <Layout>
     <div className="work">
       <p className="page-title">Work</p>
+    </div>
+    <div className="work-body">
+      <p className="big-text">We humanize brands, launch visions and clarify the complex.</p>
     </div>
   </Layout>
 )
