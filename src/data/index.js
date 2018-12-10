df9c0a0e9181be6d5c01f05d209ea34362e412ae
Jia Jing <jiajing19932@gmail.com>
2018-12-10 14:32:53 +0800

    added work data

---
+++ src/data/index.js
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    id: 1,
+    name: "CT Engineering",
+    type: ["Branding", "Website"],
+    image: "https://boygeniusreport.files.wordpress.com/2016/10/macbook-pro-2016-apple-event-1.jpg",
+    link: "https://www.ct.builders/",
+  },
+  {
+    id: 2,
+    name: "Gas Buddy Super app",
+    type: ["Branding", "UIUX"],
+    image: "https://boygeniusreport.files.wordpress.com/2016/10/macbook-pro-2016-apple-event-1.jpg",
+    link: null,
+  },
+  {
+    id: 2,
+    name: "Gas Buddy Super app",
+    type: ["Branding", "UIUX"],
+    image: "https://boygeniusreport.files.wordpress.com/2016/10/macbook-pro-2016-apple-event-1.jpg",
+    link: null,
+  }
+]
