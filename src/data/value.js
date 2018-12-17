218f13347610eed982f16c6163b000e14310f4a3
Jia Jing <jiajing19932@gmail.com>
2018-12-17 14:01:35 +0800

    feat - create value data

---
+++ src/data/value.js
@@ -0,0 +1,9 @@
+export const values = [
+  {
+    id: 1,
+    name: "Digital Tranformation & Presence",
+    description: "We design websites, applications & digital products that form the strongest connection between brand and the customers.",
+    services: ["video production", "Professional Production", "User Experience & Interface", "Ecommerce Development", "Responsive Web & Development"],
+    link: "https://www.ct.builders/",
+  },
+]
--- src/pages/value.js
+++ src/pages/value.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
+
+import { values } from '../data/value'
 import '../styles/value.scss'
 
 const ValuePage = () => (
@@ -8,25 +10,25 @@ const ValuePage = () => (
       <p className="page-title">Value</p>
     </div>
     <div className="value-body">
-      <p className="big-text">We create story, brand, digital presence, good business and sendwiches.</p>
+      <p className="big-text">We create story, brand, digital presence, good business and sandwiches.</p>
       {
-        [1,2,3,4].map((index) => {
+        values.map((value) => {
           return (
-            <div className="grid" key={index}>
+            <div className="grid" key={value.id}>
               <div className="grid-col-4">
-                <p>Digital Transformation & Presence</p>
+                <p className="value-name">{value.name}</p>
               </div>
               <div className="grid-col-6">
                 <div className="value-item">
-                  <p className="value-info">
-                    We design websites, applications & digital products that form the strongest connection between brand and the customers.
-                  </p>
+                  <p className="value-info">{value.description}</p>
                   <ul className="value-example">
-                    <li>Video Production</li>
-                    <li>Professional Production</li>
-                    <li>User Experience & Interface</li>
-                    <li>Ecommerce Development</li>
-                    <li>Responsive Web & Development</li>
+                    {
+                      value.services.map((service) => {
+                        return (
+                          <li>{ service }</li>
+                        )
+                      })
+                    }
                   </ul>
                   <button>Don't Click</button>
                 </div>
