5e1a681d603cfe2e17c4856b13b5cacb64db6306
Jia Jing <jiajing19932@gmail.com>
2018-12-18 13:41:40 +0800

    styling (index.js) - convert index page to styled component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -14,13 +14,11 @@ import { ThemeProvider } from 'styled-components';
 
 
 const theme = {
-  red: '#FF0000',
-  black: '#393939',
-  grey: '#3A3A3A',
-  lightgrey: '#E1E1E1',
-  offWhite: '#EDEDED',
+  black: '#000000',
+  gold: '#daa520',
+  goldRGB: '218, 165, 32',
   maxWidth: '1000px',
-  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+  shadow: '0 12px 24px 0 rgba(0, 0, 0, 0.1)',
 };
 
 export const wrapPageElement = ({ element, props }) => {
