a9fd6cdd4174a001c7e70123fc6a9a84fb9cae11
Jia Jing <jiajing19932@gmail.com>
2018-12-18 12:58:53 +0800

    feat (gatsby-browser) - wrap app with styled component themeProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,25 @@
 
 // Note: This could help to debug the page transition issue.
 // https://github.com/gatsbyjs/gatsby/issues/8822
+
+import React from "react"
+import { ThemeProvider } from 'styled-components';
+
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+export const wrapPageElement = ({ element, props }) => {
+  return (
+    <ThemeProvider theme={theme} {...props}>
+      {element}
+    </ThemeProvider>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import styled from 'styled-components'
 import '../styles/home.scss'
+
+const Hello = styled.div`
+  padding: 10px;
+  border-radius: 5px;
+  background-color: ${props => props.theme.lightgrey};
+  box-shadow: ${props => props.theme.bs};
+`
 
 const IndexPage = () => (
   <Layout>
+    <Hello>Hello Everybody!</Hello>
     <div className="home">
       <section>
         <p className="title-bg">We Design <span>Brand Experience</span> To Help & <span>Grow Your Business</span></p>
       </section>
-      <navbar>
+      <nav>
         <div><Link to="/value/">Value</Link></div>
         <div><Link to="/work/">Work</Link></div>
         <div><Link to="/about/">About</Link></div>
-      </navbar>
+      </nav>
     </div>
   </Layout>
 )
