8b580f514e6d42b47d7b94ee3de60640eea4a7f0
Jia Jing <jiajing19932@gmail.com>
2018-12-31 10:30:45 +0800

    fea - added google verification on head

--- src/components/head.js
+++ src/components/head.js
@@ -9,6 +9,7 @@ export default class head extends Component {
         meta={[
           { name: 'description', content: 'Besu' },
           { name: 'keywords', content: 'design agency, creative, branding' },
+          { name: 'google-site-verification', content: 'D4EYcjnP6T2COA49EO9ceEwEtq3W0Y4K8MaXmt0rWzI' },
         ]}
       >
         <html lang="en" />
